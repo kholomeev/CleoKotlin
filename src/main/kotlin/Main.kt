26a3d46f31c991ed79406f680fe0ed1a832db88f
260-23 <holomeev2012@gmail.com>
2025-09-06 15:12:35 +0300

    Выполнено задание 1.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,32 @@
-fun main(args: Array<String>) {
+import java.lang.StringBuilder
+
+fun main() {
     // Создать приложение, которое подсчитывает количество подряд идущих одинаковых символов во введенной строке.
     // На вход подается, например, строка AAADSSSRRTTHAAAA. На выходе получаем A3DS3R2T2HA4.
     // То есть, если количество подряд идущих символов меньше двух, то мы не пишем единицу.
     println("Данная программа при вводе строки определяет подряд идущие одинаковые символы и записывает число такого символа")
-    println("Введите строку из символов: "); val str = readln()
+    print("Введите строку из символов: ")
+    var input = readln()
 
-    // for (i in 0 <= .. <= str.length - 1) {
-    //     print(str[i]);
-    // }
+    if (input.isEmpty()) {
+        input = ""
+    }
 
+    val result = StringBuilder()
+    var count = 1
+
+    for (i in 1 until input.length) {
+        if (input[i] == input[i-1]) {
+            count++
+        } else {
+            result.append(input[i-1])
+            if (count > 1) result.append(count)
+            count = 1
+        }
+    }
+    // последний символ
+    result.append(input.last())
+    if (count > 1) result.append(count)
+
+    println("Итоговая строка: $result")
 }
