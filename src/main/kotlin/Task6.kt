9cfb8a337a8a67299dc1ab7ff94a98d6df1a72a1
260-23 <holomeev2011@gmail.com>
2025-09-13 14:29:36 +0300

    Выполнены задания 1-3, 5.

--- src/main/kotlin/Task6.kt
+++ src/main/kotlin/Task6.kt
@@ -1,8 +1,32 @@
+fun oddNumber(digit1: Char, digit2: Char) {
+    val result = StringBuilder()
 
+    result.append(digit1)
+    result.append(digit2)
+
+    if (result.toString().toInt() % 2 == 1)
+        print(result)
+    else {
+        result.append(digit2)
+        result.append(digit1)
+
+        if (result.toString().toInt() % 2 == 1)
+            print(result)
+        else
+            print("Создать нечётное число невозможно")
+    }
+
+}
 fun main() {
     // Создать приложение, в котором пользователь вводит две различных цифры.
     // На выходе приложение выдает, если это возможно, из введенных цифр, нечетное число. Результат выводится в консоль.
     // При невозможности создать нечетное число выводится сообщение – «Создать нечетное число невозможно».
     // Каждое число вводится на отдельной строке
+    println("Программа по составлению нечётных чисел из двух цифр(!)")
+    print("Введите первую цифру(!): ")
+    //val A = readln().toCharArray()
+    print("Введите вторую цифру(!): ")
+    //val B = readln()
 
+    //oddNumber(A, B)
 }
