ce38e0318f420c64791148365f842783280d31f5
260-23 <holomeev2011@gmail.com>
2025-09-13 13:04:57 +0300

    Выполнены задания 1-3.

--- src/main/kotlin/Task2.kt
+++ src/main/kotlin/Task2.kt
@@ -1,3 +1,18 @@
+fun countChars(input: String): String {
+    val freq = mutableMapOf<Char, Int>() // карта "символ — количество"
+    for (ch in input) {
+        freq[ch] = freq.getOrDefault(ch, 0) + 1
+    }
+    val sorted = freq.toSortedMap() // сортировка ключей по алфавиту
+
+    val result = StringBuilder()
+    for ((char, count) in sorted) {
+        result.append("$char — $count\n")
+    }
+
+    return result.toString()
+}
+
 fun main() {
     // Создать приложение, которое подсчитывает количество различных символов во введенной строке.
     // Символы в ответе расположить в алфавитном порядке. Например, дана строка AASADDSS. На выходе получаем:
@@ -6,12 +21,9 @@ fun main() {
     // S - 3
     println("Данная программа при вводе строки подсчитывает количество различных символов во введенной строке и записывает результат в столбик.")
     print("Введите строку из символов: ")
-    var input = readln()
+    val input = readln()
+    val output = countChars(input)
 
-    val freq = mutableMapOf<Char, Int>() // карта "символ — количество"
-    for (ch in input) {
-        freq[ch] = freq.getOrDefault(ch, 0) + 1
-    }
-    val sorted = freq.toSortedMap() // сортировка ключей по алфавиту
+    println("$output")
 
 }
--- src/main/kotlin/Task3.kt
+++ src/main/kotlin/Task3.kt
@@ -1,5 +1,34 @@
+import java.lang.StringBuilder
+
+fun binaryInt(number: Int): String {
+    val stack = ArrayDeque(listOf(0))
+    stack.removeLast()
+
+    var num = number
+    var i = 0
+
+    while (num != 0) {
+        val rem = num % 2
+        stack.addLast(rem)
+        num /= 2
+        i++
+    }
+
+    val result = StringBuilder()
+    while (!stack.isEmpty()) {
+        val value = stack.removeLast()
+        result.append(value)
+    }
+
+    return result.toString()
+}
 fun main() {
     // Создать приложение, которое преобразует введенное пользователем натуральное число
     // из 10-ичной системы в двоичную.
+    println("Программа преобразования чисел из десятичной в двоичную")
+    print("Введите число: ")
+    val tenth = readln()
+    val binary = binaryInt(tenth.toInt())
 
+    print("Число $tenth в двоичной системе: $binary")
 }
